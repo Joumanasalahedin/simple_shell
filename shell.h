b150fb25a37f6400a2f18e30202a54e053c47947
Thumeka <thumekatembani21@gmail.com>
2023-06-16 14:43:02 -0700

    shell.h

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+#endif /* SHELL_H */
