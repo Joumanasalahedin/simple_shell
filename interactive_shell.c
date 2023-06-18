695d383e076d593a25e5f3a5245ff57b390e4598
Thumeka <thumekatembani21@gmail.com>
2023-06-18 10:45:46 -0700

    interactive

---
+++ interactive_shell.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+/**
+ * shell_read_line - reads and allocate string with line to read in stdin
+ *
+ * Return: pointer to a string with line to content.
+ */
+
+char *shell_read_line(void)
+{
+	char *command = NULL;
+	size_t bufsize = 0;
+	ssize_t characters;
+	int status = -1;
+
+	printf("$ ");
+	characters = getline(&command, &bufsize, stdin);
+
+	if (characters = -1)
+	{
+		free(command);
+		printf("\n");
+		exit(EXIT_SUCCESS);
+	}
+	if (command[characters -1] == '\n')
+	{
+		command[characters -1] = '\0';
+	}
+
+	status = execute(command);
+	if (status == -1)
+	{
+		printf("Error, %s\n", command);
+	}
+
+	return (command);
+}
--- shell.h
+++ shell.h
@@ -1,13 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
 
+char *shell_read_line(void);
 int execute(char *command);
 
 /* helper functions */
