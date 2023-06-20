88503d2b6391e5520c468b91ee1d24fa9068829f
Joumana Arafa <joumanasalahedin@gmail.com>
2023-06-20 22:22:30 +0200

    PATH fix in execute and get_path

--- get_path.c
+++ get_path.c
@@ -12,40 +12,39 @@ char *get_path(char *command)
 {
 	char *path = getenv("PATH");
 	char *direct;
-	char *path_dup;
 	char *cmd_path;
+
+	if (access(command, X_OK) == 0)
+	{
+		return _strdup(command);
+	}
 
 	if (!path)
 	{
-		path_dup = _strdup(path);
-		direct = strtok(path_dup, ":");
-
-		while (direct != NULL)
-		{
-			cmd_path = malloc(_strlen(direct) + _strlen(command) + 2);
-			if (cmd_path == NULL)
-			{
-				perror("Error");
-				exit(EXIT_FAILURE);
-			}
-			strcpy(cmd_path, direct);
-			strcat(cmd_path, "/");
-			strcat(cmd_path, command);
-
-			if (access(cmd_path, X_OK) == 0)
-			{
-				free(path_dup);
-				return (cmd_path);
-			}
-			free(cmd_path);
-			direct = strtok(NULL, ":");
-		}
-		free(path_dup);
-
-		if (access(command, X_OK) == 0)
-		{
-			return (strdup(command));
-		}
+		return (NULL);
 	}
+
+	direct = strtok(path, ":");
+
+	while (direct != NULL)
+	{
+		cmd_path = malloc(_strlen(direct) + _strlen(command) + 2);
+		if (cmd_path == NULL)
+		{
+			perror("Error");
+			exit(EXIT_FAILURE);
+		}
+		_strcpy(cmd_path, direct);
+		_strcat(cmd_path, "/");
+		_strcat(cmd_path, command);
+
+		if (access(cmd_path, X_OK) == 0)
+		{
+			return (cmd_path);
+		}
+		free(cmd_path);
+		direct = strtok(NULL, ":");
+	}
+
 	return (NULL);
 }
