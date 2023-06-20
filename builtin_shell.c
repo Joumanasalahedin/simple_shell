a019554150b9c93caccca0156737f84dda1950d2
Thumeka <thumekatembani21@gmail.com>
2023-06-20 14:54:55 -0700

    built

---
+++ builtin_shell.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * sh_exit - exit the shell
+ *
+ * Return: 0 and exit shell
+ */
+
+int sh_exit(void)
+{
+	return(0);
+}
+
+/**
+ * sh_cd - change directory
+ * @args: arguments
+ *
+ * Return 1.
+ */
+
+int sh_cd(char **args)
+{
+	
