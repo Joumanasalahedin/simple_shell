867119ba6f3fb8ed7528c7c1cfcec1c5ffbbb329
Joumana Arafa <joumanasalahedin@gmail.com>
2023-07-01 03:54:01 +0200

    betty fix

--- execute.c
+++ execute.c
@@ -36,7 +36,7 @@ int execute(char **command, list_t *env, int num)
 		{
 			if (execve(path, command, NULL) == -1)
 			{
-				n_found(command[0], num,&env);
+				n_found(command[0], num, &env);
 				exit_c(command, env);
 			}
 		}
